b13d5c5a6b5323bd33d90841bff08a9f5057a8d4
Cuong Tran <cuong.tran@we.org>
2019-01-02 15:44:29 -0500

    feature branch

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
-    <p>Welcome to your PRODUCTION Gatsby site.</p>
+    <p>Welcome to your FEATURE/TEST Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
